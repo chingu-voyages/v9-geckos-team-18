fba53c03d3393386b5f18c23708b8342adb41083
MB <manila.bui@gmail.com>
2019-07-01 00:22:27 -0500

    fixed responsive background

--- britney-bob/src/App.js
+++ britney-bob/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import BrandBanner from "./components/BrandBanner";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/nav/Nav";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Store from "./components/Store";
-import Cart from "./components/cart/cart";
+import Store from "./components/store/Store";
+import Cart from "./components/cart/Cart";
 
 let cartData = [{ title: "project", qty: 5, price: 100 }];
 
@@ -34,34 +33,12 @@ class App extends Component {
           />
         </div>
         <Router>
-          <img
-            src="assets/main.png"
-            alt="main"
-            width="100%"
-            height="auto"
-            className="main-img"
-          />
-          <div />
           <div className="logo-img">
             <BrandBanner />
           </div>
           <div className="nav-img">
             <Nav onCartClick={this.onCartClick} />
           </div>
-          <img
-            src="assets/left-door.png"
-            alt="leftdoor"
-            width="100%"
-            height="auto"
-            className="left-door-img"
-          />
-          <img
-            src="assets/right-door.png"
-            alt="rightdoor"
-            width="100%"
-            height="auto"
-            className="right-door-img"
-          />
           <div className="content-box">
             <Route path="/about" component={About} />
             <Route path="/contact" 
