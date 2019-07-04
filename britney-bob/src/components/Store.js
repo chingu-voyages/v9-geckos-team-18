55143a24f3a9653aa2279d7ce4e3cf6e78bc3806
Himanshu Rana <himanshu.rana2111@gmail.com>
2019-07-04 22:46:53 +0530

    added add to cart function

--- britney-bob/src/components/Store.js
+++ britney-bob/src/components/Store.js
@@ -1,25 +1,34 @@
 import React from "react";
-import "./store.css"
+import "./store.css";
 
-function Store() {
+function Store(props) {
   return (
     <div className="contact-store">
-      <h2 style={{ textAlign: "center" }}>
-        Store
-      </h2>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <h2 style={{ textAlign: "center" }}>Store</h2>
+      <div className="row">
+        {props.data.map((obj, i) => {
+          return renderItem(obj, i, props);
+        })}
+      </div>
     </div>
   );
 }
+
+const renderItem = (obj, i, props) => {
+  return (
+    <div
+      className="col-md-4"
+      key={i}
+      style={{ textAlign: "center", margin: "10px 0" }}
+    >
+      <div style={{ width: "100%", height: "auto" }}>
+        <img src={obj.url} height="auto" width="100%" />
+      </div>
+      <div>{obj.title}</div>
+      <div>{obj.price}</div>
+      <button onClick={() => props.addToCart(obj)}>ADD TO CART</button>
+    </div>
+  );
+};
 
 export default Store;
