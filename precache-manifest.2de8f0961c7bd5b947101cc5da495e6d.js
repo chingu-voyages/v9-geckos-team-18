9d3201e32100eac8c47c081f96e1ddf66f7cec93
Himanshu Rana <himanshu.rana2111@gmail.com>
2019-07-15 23:14:56 +0530

    Updates

---
+++ precache-manifest.2de8f0961c7bd5b947101cc5da495e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f49ab02a1e693577c2610831e6aef7",
+    "url": "/v9-geckos-team-18/index.html"
+  },
+  {
+    "revision": "b5b58d27ae8fad4772d7",
+    "url": "/v9-geckos-team-18/static/css/main.835bbf79.chunk.css"
+  },
+  {
+    "revision": "5831dd1da1c2fb77a626",
+    "url": "/v9-geckos-team-18/static/js/2.15b364e9.chunk.js"
+  },
+  {
+    "revision": "b5b58d27ae8fad4772d7",
+    "url": "/v9-geckos-team-18/static/js/main.dc4a913f.chunk.js"
+  },
+  {
+    "revision": "6126c082996dd4a02b6c",
+    "url": "/v9-geckos-team-18/static/js/runtime~main.d46b4224.js"
+  }
+]);
